5a2d8f3e7badf74e52a946a936d7250920006bc2
Gaoussou Doucoure <106108233+GaoussouDoucoure@users.noreply.github.com>
2022-07-11 13:33:22 -0700

    Update taskManager.js
    add task manager and console log them. And test it out.

--- js/taskManager.js
+++ js/taskManager.js
@@ -1,5 +1,21 @@
-class TaskManager {
-  constructor () {
-    this._tasks = [];
+class TaskManager  {
+  constructor (currentId) {
+    this.tasks = [];
+    this.currentId = 0;
   }
+addTask(name, description, assignedTo, dueDate, status) {
+  const task = {
+    id: this.currentId++,
+    name: name,
+    description: description,
+    assignedTo: assignedTo,
+    dueDate: dueDate,
+    status: 'ToDo',
+  };
+  
+    this.tasks.push(task);
+    console.log(this.tasks);
+}
+
 };
+
