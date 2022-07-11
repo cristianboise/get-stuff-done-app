d1710b4603bea94e8452cc695e38ff96692e2d12
Vincent Bradbury <vincent.bradbury223@gmail.com>
2022-07-11 16:36:41 -0500

    Add newTask instance and submit form listeners

--- js/taskManager.js
+++ js/taskManager.js
@@ -3,19 +3,29 @@ class TaskManager  {
     this.tasks = [];
     this.currentId = 0;
   }
-addTask(name, description, assignedTo, dueDate, status) {
-  const task = {
-    id: this.currentId++,
-    name: name,
-    description: description,
-    assignedTo: assignedTo,
-    dueDate: dueDate,
-    status: 'ToDo',
-  };
-  
-    this.tasks.push(task);
-    console.log(this.tasks);
-}
+  addTask(taskName, description, assignedTo, dueDate, status) {
+    const task = {
+      id: this.currentId++,
+      taskName: taskName,
+      description: description,
+      assignedTo: assignedTo,
+      dueDate: dueDate,
+      status: 'ToDo',
+    };
+
+      this.tasks.push(task);
+      console.log(this.tasks);
+  }
 
 };
+const newTask = new TaskManager()
+
+let submitForm = document.getElementById('submit-form');
+submitForm.addEventListener('submit', () => {
+  newTask.addTask(
+    document.getElementByID("taskName").value,
+    document.getElementByID("description").value,
+    document.getElementByID("assignedTo").value,
+    document.getElementByID("dueDate").value
+})
 
