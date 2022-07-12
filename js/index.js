669ef56da49eb16345b21d6a7182b9f76ddf2e0c
Yugen <woozy912@gmail.com>
2022-07-12 14:52:36 -0400

    troubleshot render function, moved code validation

--- js/index.js
+++ js/index.js
@@ -1,7 +1,28 @@
 document.getElementById("submit-form").addEventListener("submit", function(event) {
 
-        console.log('Working!');
         event.preventDefault();
-        newTask.addTask();
+        console.log('Working!');
+        let taskNameValue = document.getElementById("taskName").value
+        let descriptionValue = document.getElementById("description").value
+        let assignedToValue = document.getElementById("assignedTo").value
+        let dueDateValue = document.getElementById("dueDate").value
+        // form validation
+        if ((taskNameValue !== '') && (descriptionValue !== '') &&
+            (assignedToValue !== '') && (dueDateValue !== '')) {
+        newTask.addTask(
+            taskNameValue,
+            descriptionValue,
+            assignedToValue,
+            dueDateValue
+        )
+        console.log(newTask.tasks)
+        // clear the fields for new task submission
+        document.getElementById("taskName").value = ''
+        document.getElementById("description").value = ''
+        document.getElementById("assignedTo").value = ''
+        document.getElementById("dueDate").value = ''
+        } else {
+            alert('Form is either incomplete or invalid.  Please try again.')
+        }
         newTask.render();
     });
--- js/taskManager.js
+++ js/taskManager.js
@@ -49,32 +49,3 @@ class TaskManager  {
 };
 // make new instance of the TaskManager to store the tasks array and .addTask() method
 const newTask = new TaskManager()
-// submit button functionality
-let submitButton = document.getElementById('submitButton');
-submitButton.addEventListener('click', (event) => {
-    event.preventDefault();
-    let taskNameValue = document.getElementById("taskName").value
-    let descriptionValue = document.getElementById("description").value
-    let assignedToValue = document.getElementById("assignedTo").value
-    let dueDateValue = document.getElementById("dueDate").value
-    // form validation
-    if ((taskNameValue !== '') && (descriptionValue !== '') &&
-      (assignedToValue !== '') && (dueDateValue !== '')) {
-      newTask.addTask(
-        taskNameValue,
-        descriptionValue,
-        assignedToValue,
-        dueDateValue
-      )
-      console.log(newTask.tasks)
-      // clear the fields for new task submission
-      document.getElementById("taskName").value = ''
-      document.getElementById("description").value = ''
-      document.getElementById("assignedTo").value = ''
-      document.getElementById("dueDate").value = ''
-    } else {
-      alert('Form is either incomplete or invalid.  Please try again.')
-    }
-  }
-)
-
